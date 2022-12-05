4020a9a14eb09809db15bf4a018bdbadc0d34733
Mohamed Inzamam <94120089+inzi95@users.noreply.github.com>
2022-12-05 12:22:30 +0400

    Update ecom_queries.sql

--- ecom_queries.sql
+++ ecom_queries.sql
@@ -17,3 +17,37 @@ GROUP BY
   order_date
 ORDER BY
   order_date desc
+
+-- Counting the number of orders per UserID
+SELECT
+  o.user_id,
+  COUNT(o.user_id) as number_of_orders
+FROM
+  `bigquery-public-data.thelook_ecommerce.orders` AS o
+GROUP BY
+  o.user_id
+ORDER BY
+  2 desc
+ 
+--  Inner Joining the top Selling Products
+WITH
+  top_products AS (
+  SELECT
+    oi.product_id,
+    COUNT(product_id) AS number_of_orders,
+  FROM
+    `bigquery-public-data.thelook_ecommerce.order_items` AS oi
+  GROUP BY
+    product_id
+  ORDER BY
+    2 DESC)
+SELECT
+  p.*
+FROM
+  `bigquery-public-data.thelook_ecommerce.products` AS p
+INNER JOIN
+  top_products
+ON
+  top_products.product_id = p.id
+WHERE
+  top_products.number_of_orders > 10
